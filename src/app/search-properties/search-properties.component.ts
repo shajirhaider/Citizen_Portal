eeffff4e94ff65ecb5c313a2aacbcaf8bad0d92d
shajir.haider <shajirhaider04@gmail.com>
2019-03-09 12:47:54 +0600

    feedback resolved 05/03/19

--- src/app/search-properties/search-properties.component.ts
+++ src/app/search-properties/search-properties.component.ts
@@ -14,14 +14,18 @@ export class SearchPropertiesComponent implements OnInit {
   };
   streetType: Object = [];
   streetDirection: Object = [];
+  streets: Object = [];
   searchResultshow: boolean = false;
+  msgShow: boolean = false;
   searchResult: Object = [];
 constructor(private httpService: HttpService) {}
 
   ngOnInit() {
     this.getValidStreetType()
     this.getValidStreetDirections()
+    this.getValidStreet()
   }
+
   getValidStreetType(){
     let url = 'getValidStreetTypes'
     let body = {
@@ -50,29 +54,54 @@ constructor(private httpService: HttpService) {}
         },
         (error) => console.log(error)
       );
-  }
-  searchProperty(){
-    let url = 'searchProperty'
-    let body = this.searchProperties
+  } 
+  getValidStreet(){
+    let url = 'getValidStreet'
+    let body = {
+      "token":"amandaportal", 
+      "lid":""
+    }
     this.httpService.post(url,body)
       .subscribe(
         (response) =>{
-          console.log(response)
-          if(response.status == 200){
-            this.searchResultshow = true
-            this.searchResult = response["body"][0]
-          }
-         
+          console.log(response["body"])
+          this.streets = response["body"]
         },
         (error) => console.log(error)
       );
+  }
+  searchProperty(){
+    if( !this.searchProperties["streetNumber"] && !this.searchProperties["streetName"] && !this.searchProperties["streetType"] &&
+      !this.searchProperties["streetDirection"] && !this.searchProperties["unitNumber"] && !this.searchProperties["rollNumber"]){
+      this.msgShow = true;
+      this.searchResultshow = false;
+      console.log("msg",this.searchProperties["streetNumber"])
+    }else{
+      let url = 'searchProperty'
+      let body = this.searchProperties
+      this.httpService.post(url,body)
+        .subscribe(
+          (response) =>{
+            console.log("res",response)
+            if(response.status == 200){
+              this.searchResultshow = true
+              this.msgShow = false
+              this.searchResult = response["body"][0]
+            }
+           
+          },
+          (error) => console.log(error)
+        );
+
+    }
   }
   clear(){
     this.searchProperties = {};
   }
   searchShow(){
-    this.searchResultshow = false
-    this.searchProperties = {}
+    this.searchResultshow = false,
+    this.msgShow = false
+   // this.searchProperties = {}
   }
 
 }
